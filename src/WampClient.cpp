475110e5431cdc55169ecf41a40701e4f673786d
cella <chris.ella@accesso.com>
2016-06-20 14:56:36 +0100

    Implementing many of the WampClient methods

--- src/WampClient.cpp
+++ src/WampClient.cpp
@@ -1,7 +1,7 @@
 #include "WampClient.h"
 #include <ArduinoJson.h>
 
-#include "MessageCodes.h"
+#include <stdlib.h>
 
 WampClient::WampClient()
 {
@@ -13,12 +13,11 @@ WampClient::~WampClient()
 
 }
 
+// TODO: Deal with messages based on the current state etc
 void WampClient::onMessage (const char * str)
 {
-    // TODO: Deal with messages based on the current state etc
-    // TODO: Copy string param into a non const version as Json can/will edit it
+    char * cpStr = strdup(str); // Copy str into an mutatable char *
     DynamicJsonBuffer jsonBuffer;
-    // TODO: Maybe switch to a static buffer if we can assure a max required size
     JsonArray& root = jsonBuffer.parseArray(str);
     if (!root.success())
     {
@@ -29,7 +28,16 @@ void WampClient::onMessage (const char * str)
     switch (command)
     {
         case MessageCodes::WELCOME:
-            OnWelcome();
+            OnWelcome((int)root[1], root[2]);
+            break;
+        case MessageCodes::ABORT:
+            OnAbort();
+            break;
+        case MessageCodes::GOODBYE:
+            OnGoodbye();
+            break;
+        case MessageCodes::PUBLISHED:
+            OnPublished((int)root[1], (int)root[2]);
             break;
     }
 }
@@ -50,14 +58,8 @@ void WampClient::Hello(const char * realm)
     JsonArray& root = jsonBuffer.createArray();
     root.add((int)MessageCodes::HELLO);
     root.add(realm);
-    JsonObject& rolesObj = jsonBuffer.createObject();
-    rolesObj["publisher"] = jsonBuffer.createObject();
-    rolesObj["subscriber"] = jsonBuffer.createObject();
-    root.add(rolesObj);
-    char helloRequest[200];
-    root.printTo(helloRequest, sizeof(helloRequest));
-    // TODO: Skip the copy to buffer and use with .print() straight to a stream
-    _sendMessage(helloRequest);
+    root.add(GenerateRolesObject(jsonBuffer));
+    SendJson(root);
     // TODO: Detect if the send failed in some way to prevent going into
     // awaiting state if so.
     _state = WampState::AWAITING_WELCOME;
@@ -73,44 +75,98 @@ void WampClient::Goodbye()
 
 }
 
-void WampClient::Error()
+void WampClient::Publish(JsonObject& options, const char * topic, JsonArray * args)
 {
-
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonArray& root = jsonBuffer.createArray();
+    root.add((int)MessageCodes::PUBLISH);
+    root.add(GenerateRequestId());
+    root.add(options);
+    root.add(topic);
+    if (args != NULL)
+    {
+        root.add(*args);
+    }
+    SendJson(root);
+    _state = WampState::AWAITING_PUBLISHED;
 }
 
-void WampClient::Publish()
+void WampClient::Subscribe(JsonObject& options, const char * topic)
 {
-
+    StaticJsonBuffer<200> b;
+    JsonArray& r = b.createArray();
+    r.add((int)MessageCodes::SUBSCRIBE);
+    r.add(GenerateRequestId());
+    r.add(options);
+    r.add(topic);
+    SendJson(r);
+    _state = WampState::AWAITING_SUBSCRIBED;
 }
 
-void WampClient::Subscribe()
+void WampClient::Unsubscribe(int subId)
 {
-
+    StaticJsonBuffer<200> b;
+    JsonArray& r = b.createArray();
+    r.add((int)MessageCodes::UNSUBSCRIBE);
+    r.add(GenerateRequestId());
+    r.add(subId);
+    SendJson(r);
+    _state = WampState::AWAITING_UNSUBSCRIBED;
 }
 
-void WampClient::Unsubscribe()
+void WampClient::Call(JsonObject& options, const char * procedure, JsonArray * args)
 {
-
+    StaticJsonBuffer<200> b;
+    JsonArray& r = b.createArray();
+    r.add((int)MessageCodes::CALL);
+    r.add(GenerateRequestId());
+    r.add(options);
+    r.add(procedure);
+    if (args != NULL)
+    {
+        r.add(*args);
+    }
+    SendJson(r);
+    _state = WampState::AWAITING_CALL_RESULT;
 }
 
-void WampClient::Call()
+void WampClient::Register(JsonObject& options, const char * procedure)
 {
-
+    StaticJsonBuffer<200> b;
+    JsonArray& r = b.createArray();
+    r.add((int)MessageCodes::REGISTER);
+    r.add(GenerateRequestId());
+    r.add(options);
+    r.add(procedure);
+    SendJson(r);
+    _state = WampState::AWAITING_REGISTERED;
 }
 
-void WampClient::Register()
+void WampClient::Unregister(int regId)
 {
-
+    StaticJsonBuffer<200> b;
+    JsonArray& r = b.createArray();
+    r.add((int)MessageCodes::UNREGISTER);
+    r.add(GenerateRequestId());
+    r.add(regId);
+    SendJson(r);
+    _state = WampState::AWAITING_UNREGISTERED;
 }
 
-void WampClient::Unregister()
+void WampClient::OnError(MessageCodes code)
 {
-
+    // TODO: Handle different errors based on the _state and the incoming error
 }
 
-void WampClient::OnWelcome()
+void WampClient::OnWelcome(int sessionId, JsonObject& detailsDict)
 {
-
+    if (_state != WampState::AWAITING_WELCOME)
+    {
+        // Invalid, A welcome MUST be the response to a HELLO
+        // TODO: Disconnect and fail?
+        return;
+    }
+    // TODO: Process sessionId and details dictionary
 }
 
 void WampClient::OnAbort()
@@ -123,19 +179,34 @@ void WampClient::OnGoodbye()
 
 }
 
-void WampClient::OnPublished()
+void WampClient::OnPublished(int publishRequestId, int publicationId)
 {
-
+    if (_state != WampState::AWAITING_PUBLISHED)
+    {
+        // TODO: Check spec, how to handle unexpected message ordering
+        return;
+    }
+    // TODO: Compare the PublishRequestId to the original request somehow. Requires saving it somewhere temporarily
 }
 
-void WampClient::OnSubscribed()
+void WampClient::OnSubscribed(int subRequestId, int subId)
 {
-
+    if (_state != WampState::AWAITING_SUBSCRIBED)
+    {
+        // TODO: Check spec, how to handle unexpected message ordering
+        return;
+    }
+    // TODO: Save the subId somewhere so we know we're subscribed and it's available for Unsubscribe if required (Save as topic -> subId dictionary/map ?)
 }
 
-void WampClient::OnUnsubscribed()
+void WampClient::OnUnsubscribed(int unsubRequestId)
 {
-
+    if (_state != WampState::AWAITING_UNSUBSCRIBED)
+    {
+        // TODO: Check spec, how to handle unexpected message ordering
+        return;
+    }
+    // TODO: Remove subscription from local dictionary/map
 }
 
 void WampClient::OnResult()
@@ -143,12 +214,53 @@ void WampClient::OnResult()
 
 }
 
-void WampClient::OnRegistered()
+void WampClient::OnRegistered(int registeredId)
 {
-
+    if (_state != WampState::AWAITING_REGISTERED)
+    {
+        // TODO: Check spec, how to handle unexpected message ordering
+        return;
+    }
+    // TODO: Save the regId to a dictionary/map for later unregistering
 }
 
 void WampClient::OnUnregistered()
 {
+    if (_state != WampState::AWAITING_UNREGISTERED)
+    {
+        // TODO: Check spec, how to handle unexpected message ordering
+        return;
+    }
+// TODO: Remove the registered procedure id from the local dicationary/map
+}
 
+int WampClient::GenerateRequestId()
+{
+    return rand();
+}
+
+void WampClient::SendJson(JsonObject& obj)
+{
+    char b[512]; // TODO: This may need to be increased ?
+    obj.printTo(b, sizeof(b));
+    _sendMessage(b);
+}
+
+void WampClient::SendJson(JsonArray& arr)
+{
+    char b[512]; // TODO: This may need to be increased ?
+    arr.printTo(b, sizeof(b));
+    _sendMessage(b);
+}
+
+void WampClient::SendString(const char * str)
+{
+    _sendMessage(str);
+}
+
+JsonObject& WampClient::GenerateRolesObject(JsonBuffer& buffer)
+{
+    JsonObject& o = buffer.createObject();
+    // TODO: Add to roles object
+    return o;
 }
--- src/WampClient.h
+++ src/WampClient.h
@@ -4,7 +4,10 @@
 #include <Arduino.h>
 #include <functional>
 
+#include <ArduinoJson.h>
+
 #include "WampState.h"
+#include "MessageCodes.h"
 
 class WampClient {
 public:
@@ -20,29 +23,36 @@ public:
     void Hello(const char * realm); // Awaits Welcome
     void Abort();
     void Goodbye();
-    void Error();
-    void Publish(); // Awaits Published
-    void Subscribe();  // Awaits Subscribed
-    void Unsubscribe(); // Awaits Unsubscribed
-    void Call(); // Awaits Result
-    void Register(); // Awaits Registered
-    void Unregister(); // Awaits Unregistered
+    void Publish(JsonObject& options, const char * topic, JsonArray * args = NULL); // Awaits Published
+    void Subscribe(JsonObject& options, const char * topic);  // Awaits Subscribed
+    void Unsubscribe(int subId); // Awaits Unsubscribed
+    void Call(JsonObject& options, const char * procedure, JsonArray * args = NULL); // Awaits Result
+    void Register(JsonObject& options, const char * procedure); // Awaits Registered
+    void Unregister(int regId); // Awaits Unregistered
 
 private:
 
-    void OnWelcome();
+    void OnError(MessageCodes code);
+    void OnWelcome(int sessionId, JsonObject& detailsDict);
     void OnAbort();
     void OnGoodbye();
-    void OnPublished();
-    void OnSubscribed();
-    void OnUnsubscribed();
+    void OnPublished(int publishRequestId, int publicationId);
+    void OnSubscribed(int subRequestId, int subId);
+    void OnUnsubscribed(int unsubRequestId);
     void OnResult();
-    void OnRegistered();
+    void OnRegistered(int registeredId);
     void OnUnregistered();
+
+    JsonObject& GenerateRolesObject(JsonBuffer& buffer);
 
 private:
     WampState _state;
 
+    int GenerateRequestId();
+
+    void SendJson(JsonObject& obj);
+    void SendJson(JsonArray& arr);
+    void SendString(const char * str);
     std::function<void(const char *)> _sendMessage;
 };
 
